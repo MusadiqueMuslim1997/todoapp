6260b3677c0206a2570e3b591f41b35a161659e5
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-08-09 15:07:50 +0500

    add new navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,45 +3,24 @@ import React, { useState } from 'react';
 import './Style.css'; // Ensure you have a CSS file for styling
 
 const NavBar = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleToggle = () => {
-    setIsChecked(!isChecked);
-  };
+ 
 
   return (
-    <nav>
-      <div className="logo">Resturant</div>
-      <input
-        type="checkbox"
-        id="check"
-        checked={isChecked}
-        onChange={handleToggle}
-        style={{ display: 'none' }} // Hide the checkbox itself
-      />
-      <label
-        htmlFor="check"
-        className="exit"
-        onClick={() => setIsChecked(false)}
-      >
-        <i className="fa-solid fa-xmark"></i>
-      </label>
-      <label
-        htmlFor="check"
-        className="check"
-        onClick={() => setIsChecked(true)}
-      >
-        <i className="fa-solid fa-bars"></i>
-      </label>
+   <>
+   <header>
+  <div className="logo">Resturant</div>
+<div className="list">
+  <ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+    <li className='items'>Menu</li>
 
-      <div className=  {`ul ${isChecked ? 'show' : ''}`} id="ul">
-        <ul>
-          <li>Home</li>
-          <li>Project</li>
-          <li> Contact Us </li>
-        </ul>
-      </div>
-    </nav>
+  </ul>
+</div>
+   </header>
+   
+   </>
   );
 };
 
