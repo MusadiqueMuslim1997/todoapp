0902d0d3528a3f28ef34dc0f9ff70a05b64364b2
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-08-10 09:05:29 +0500

    add back button

--- src/pages/Chikcken.jsx
+++ src/pages/Chikcken.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+
 import "../components/Style.css";
 import { img1 } from '../components/Cards';
+
 
 const Chikcken = () => {
    const [show, setShow] =useState(false);
@@ -25,6 +28,7 @@ const Chikcken = () => {
       <div className="itemBtn">
 
         <button onClick={handleShow}>View Detail</button>
+        <Link to = '/'><button>Go Back</button></Link>
       </div>
        
      {show &&  <div className="description">
